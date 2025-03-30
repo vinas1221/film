31e7d62b4abf471d16d74e6eb4003cacf586a060
vinas1221 <benjo_vinas@yahoo.com>
2025-03-30 21:07:52 +0000

    Improve configuration - removing deprecated function

--- embetty/src/embed/tweet.test.ts
+++ embetty/src/embed/tweet.test.ts
@@ -15,8 +15,8 @@ beforeEach(() => {
 
 describe.skip('Tweet', () => {
   it('should fetch remote data', async () => {
-    const spy = await getFetchSpy(Tweets.text)
-    const { element } = await createTweet(Tweets.text)
+    let spy = await getFetchSpy(Tweets.text)
+    let { element } = await createTweet(Tweets.text)
     expect(element._data?.data.id).toEqual(Tweets.text)
     expect(element._fetched).toBeTruthy()
     expect(spy).toHaveBeenCalledWith('/tweet/934029337019416579')
@@ -24,7 +24,7 @@ describe.skip('Tweet', () => {
 
   it('should generate the tweet header', async () => {
     await getFetchSpy(Tweets.text)
-    const { query } = await createTweet(Tweets.text)
+    let { query } = await createTweet(Tweets.text)
     expect(query('header img')).toBeTruthy()
     expect(query('header span strong')).toBeTruthy()
     expect(query('header span a')).toBeTruthy()
@@ -32,7 +32,7 @@ describe.skip('Tweet', () => {
 
   it('should generate the tweet body', async () => {
     await getFetchSpy(Tweets.text)
-    const { query } = await createTweet(Tweets.text)
+    let { query } = await createTweet(Tweets.text)
     expect(query('article')).toBeTruthy()
     expect(query('article p')).toBeTruthy()
     expect(query('article time')).toBeTruthy()
@@ -40,23 +40,23 @@ describe.skip('Tweet', () => {
 
   it('should show images', async () => {
     await getFetchSpy(Tweets.image)
-    const { query } = await createTweet(Tweets.image)
+    let { query } = await createTweet(Tweets.image)
     expect(query('#media img')).toBeTruthy()
   })
 
   it('should contain a time tag', async () => {
     await getFetchSpy(Tweets.text)
-    const { query } = await createTweet(Tweets.text)
-    const time = query('time')
-    const datePattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z$/
+    let { query } = await createTweet(Tweets.text)
+    let time = query('time')
+    let datePattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z$/
     expect(datePattern.test(time.getAttribute('datetime')!)).toBeTruthy()
     expect(time.textContent?.length).toBeGreaterThan(5)
   })
 
   it('a[target=_blank] should have a [rel=noopener] attribute', async () => {
     await getFetchSpy(Tweets.link)
-    const { element } = await createTweet(Tweets.link)
-    const links = [...element.shadowRoot!.querySelectorAll('a[target=_blank')]
+    let { element } = await createTweet(Tweets.link)
+    let links = [...element.shadowRoot!.querySelectorAll('a[target=_blank')]
     expect(links).toHaveLength(4)
 
     links.forEach((link) => {
@@ -66,8 +66,8 @@ describe.skip('Tweet', () => {
 
   it('tweet link should contain the username and the tweet id', async () => {
     await getFetchSpy(Tweets.text)
-    const { element } = await createTweet(Tweets.text)
-    const links = [...element.shadowRoot!.querySelectorAll('.tweet__link')]
+    let { element } = await createTweet(Tweets.text)
+    let links = [...element.shadowRoot!.querySelectorAll('.tweet__link')]
     expect(links).toHaveLength(1)
     // @ts-ignore
     expect(links[0].getAttribute('href')).toBe(
@@ -78,8 +78,8 @@ describe.skip('Tweet', () => {
   describe('Tweet body', () => {
     it('should replace hash tags', async () => {
       await getFetchSpy(Tweets.text)
-      const { query } = await createTweet(Tweets.text)
-      const a = query(
+      let { query } = await createTweet(Tweets.text)
+      let a = query(
         'article a[href="https://twitter.com/hashtag/Wochenende"]',
       )
       expect(a.textContent).toBe('#Wochenende')
@@ -87,30 +87,30 @@ describe.skip('Tweet', () => {
 
     it('should replace @user', async () => {
       await getFetchSpy(Tweets.answeredTweet)
-      const { query, element } = await createTweet(Tweets.answeredTweet)
-      const a = query('article a[href="https://twitter.com/VictorVEnciso"]')
+      let { query, element } = await createTweet(Tweets.answeredTweet)
+      let a = query('article a[href="https://twitter.com/VictorVEnciso"]')
       expect(element.shadowRoot?.innerHTML).toMatchSnapshot()
       expect(a.textContent).toBe('@VictorVEnciso')
     })
 
     it('should not output the image link when displaying the image', async () => {
       await getFetchSpy(Tweets.image)
-      const { element } = await createTweet(Tweets.image)
-      const text = element.fullText
+      let { element } = await createTweet(Tweets.image)
+      let text = element.fullText
       expect(text).not.toContain('https://t.co/wLy5Asq3z0')
     })
 
     it('should show tweet only without include-thread attribute', async () => {
-      const { element } = await createTweet(Tweets.reply)
+      let { element } = await createTweet(Tweets.reply)
       expect(element.shadowRoot!.querySelector('embetty-tweet')).toBeNull()
     })
 
     it('should show thread and inherit include-thread attribute on being set', async () => {
       await getFetchSpy(Tweets.reply)
-      const { element } = await createTweet(Tweets.reply, {
+      let { element } = await createTweet(Tweets.reply, {
         'include-thread': '',
       })
-      const parent = element.shadowRoot!.querySelector('embetty-tweet')
+      let parent = element.shadowRoot!.querySelector('embetty-tweet')
       expect(parent?.hasAttribute('include-thread')).toBeTruthy()
     })
   })
